0cb40d59b12ed274e79eda82f6cfba94bcd2972d
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-15 02:26:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,10 @@ def take_a_number(array, name)
   array << name
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift}"
+end
