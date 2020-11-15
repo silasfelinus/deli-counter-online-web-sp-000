6bef9c7dac375cd91e3e08faed6d81af584eff93
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-15 02:26:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,5 +20,5 @@ def now_serving(array)
   if array.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{array.shift}"
+    puts "Currently serving #{array.shift}."
 end
