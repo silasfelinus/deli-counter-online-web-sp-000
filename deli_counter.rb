d5c99a4dcc7f7739cc53ea013213841d902b7cd8
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-15 01:37:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(array)
+  if array.length == 0
+    "The line is currently empty."
+  else 
+    sentence = "The line is currently: "
+    array.each_with_index do |name, index|
+      sentence << "#{index+1}, #{name}. "
+    end 
+  end
+end
+  
+    
