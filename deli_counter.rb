d76eb66e4a35da96f9230f718963a4c3e3e51d96
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-15 02:09:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    sentence = "The line is currently: "
+    sentence = "The line is currently:"
     array.each_with_index do |name, index|
-      sentence << "#{index+1}. #{name}"
+      sentence << " #{index+1}. #{name}"
     end
     puts sentence
   end
